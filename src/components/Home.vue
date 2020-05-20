3bf90ebb84f71cb26779361afe38e8fe0496fd4e
itcast <itheima@itcast.cn>
2020-05-20 22:05:53 +0800

    home组件的第一次修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
+ <!-- <div>
     <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  </div> -->
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>Header</el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右边主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
