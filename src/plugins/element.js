f3731205e9990ce2df04426b2f4e6dbdf377e91d
itcast <itheima@itcast.cn>
2020-05-28 11:30:01 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox,
+  Upload
 } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
@@ -65,3 +70,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
